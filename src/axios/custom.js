0d45496a84a5af3aa01031a182da159b0ae6f267
kriz1999 <muhammadkriz22@gmail.com>
2024-01-31 09:33:56 -0600

    new key

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -2,14 +2,14 @@
 
 export const config = {
   headers: {
-    "X-RapidAPI-Key": "46d1ecb7a1msh14eaf3a89d35946p1a3a43jsna330750a82e7",
+    "X-RapidAPI-Key": "a51271edbdmsh501614c26dc16edp1fee53jsnff9f5b4b8d14",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
 export const config2 = {
   params: { limit: "100" },
   headers: {
-    "X-RapidAPI-Key": "46d1ecb7a1msh14eaf3a89d35946p1a3a43jsna330750a82e7",
+    "X-RapidAPI-Key": "a51271edbdmsh501614c26dc16edp1fee53jsnff9f5b4b8d14",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -17,19 +17,7 @@ export const youtobeOp = {
   method: "GET",
 
   headers: {
-    "X-RapidAPI-Key": "46d1ecb7a1msh14eaf3a89d35946p1a3a43jsna330750a82e7",
+    "X-RapidAPI-Key": "a51271edbdmsh501614c26dc16edp1fee53jsnff9f5b4b8d14",
     "X-RapidAPI-Host": "youtube-v3-alternative.p.rapidapi.com",
   },
 };
-
-// export
-//  const options = {
-//   method: "GET",
-//   url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
-//   headers: {
-//     "X-RapidAPI-Key": "6eee0781bbmshb4200d64bc48ceap1b8676jsnb0f3a579a508",
-//     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-//   },
-// };
-
-// export default options;
